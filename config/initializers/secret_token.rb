5aecd39edf272d25b62786f2ea1743a5016630f6
Alexandru Marinescu <alex.marinescu90@gmail.com>
2013-04-20 19:26:04 +0300

    Deleted rails files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Pidj::Application.config.secret_token = '1a44fe62e328878433012646f415f8f1c650ba42b8963dd33b845d0758e2e3b2e934991c86d5349670a3e78d2f2c61e43d874ec98698cabd7c7227b5646c0c65'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Pidj::Application.config.session_store :cookie_store, key: '_pidj_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Pidj::Application.config.session_store :active_record_store
